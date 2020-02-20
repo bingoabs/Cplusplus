a500d579d364ed9d9c06e95e2269251c3190a685
BingoZhou <bingoabs@gmail.com>
2020-02-20 23:29:03 +0800

    Simple vector

--- main.cc
+++ main.cc
@@ -11,14 +11,24 @@ using std::endl;
 int main()
 {
     string s("some string");
+    // upper the first letter in the string
     if(s.begin() == s.end())
     {
         auto it = s.begin();
         *it = toupper(*it);
     };
-
+    // upper the first word in the string
     for (auto it = s.begin(); it != s.end() && !isspace(*it); ++it)
     {
         *it = toupper(*it);
-    }
+    };
+    // (*it).empty() == it->empty
+    vector<string> text;
+    text.push_back("first word");
+    text.push_back(" ");
+    text.push_back("second word");
+    for(auto it = text.cbegin();
+    it != text.cend() && !it->empty(); ++it)
+        cout << *it << endl;
+    
 }
