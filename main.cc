7d680f0afd07b29af0d488d027f3751bb2867766
BingoZhou <bingoabs@gmail.com>
2020-02-20 23:52:57 +0800

    binary search algorithm

--- main.cc
+++ main.cc
@@ -10,13 +10,19 @@ using std::endl;
 
 int main()
 {
-    vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for(auto &n : nums)
+    vector<int> sorted_nums = {0, 10, 20, 20, 30, 40, 50, 60, 70, 80, 90};
+    int sought = 10;
+    auto beg = sorted_nums.begin(), end = sorted_nums.end();
+    auto mid = sorted_nums.begin() + (end - beg)/2;
+    while(mid != end && *mid != sought)
     {
-        n = n * 2;
-    };
-    for(auto n : nums)
-    {
-        cout << n << endl;
-    };
+        if(sought < *mid)
+        {
+            end = mid;
+        } else {
+            beg = mid + 1;
+        };
+        mid = beg + (end - beg)/2;
+        cout << "one loop" << endl;
+    }
 }
