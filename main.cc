986972aac9b7bf029af4c99235e37ca615a71544
BingoZhou <bingoabs@gmail.com>
2020-02-21 17:26:00 +0800

    fourth array example

--- main.cc
+++ main.cc
@@ -10,15 +10,15 @@ using std::endl;
 
 int main()
 {
-    int a[10] = {};
-    int beg = 0;
-    for(int &e : a)
-    {
-        e = beg++;
-    };
-    for(int e : a)
-    {
-        cout << e << " ";
-    };
-    cout << endl;
+    string nums[] = {"one", "two", "three"};
+    string *p = &nums[0];
+
+    string *p2 = nums; // equals to p2 = &nums[0];
+
+    int ia[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    auto ia2(ia); // equals to int *ia2 = &ia[0];
+
+    decltype(ia) ia3 = {1, 1, 1, 1, 1, 1, 1};
+    // equals to int ia3[] = {...};
+    ia3[4] = 10;
 }
