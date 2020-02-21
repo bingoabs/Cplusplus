d877b2396642983c518d32a1124400fe872d39ff
BingoZhou <bingoabs@gmail.com>
2020-02-21 23:12:17 +0800

    replace uto to real type

--- main.cc
+++ main.cc
@@ -10,21 +10,21 @@ using std::endl;
 
 int main()
 {
-    int ia[3][4];
+    int ia[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     int (*p)[4] = ia;
     p = &ia[2];
 
-    for (auto p = ia; p != ia + 3; ++p)
+    for (int (*p)[4] = ia; p != ia + 3; ++p)
     {
-        for(auto q = *p; q != *p + 4; ++q)
+        for(int *q = *p; q != *p + 4; ++q)
             cout << *q << ' ';
         cout << endl;
     };
 
-    for (auto p = begin(ia); p != end(ia); ++p)
-    {
-        for (auto q = begin(*p); q != en(*p); ++q)
-            cout << *q << ' ';
-        cout << endl;
-    };
+    // for (auto p = begin(ia); p != end(ia); ++p)
+    // {
+    //     for (auto q = begin(*p); q != en(*p); ++q)
+    //         cout << *q << ' ';
+    //     cout << endl;
+    // };
 }
