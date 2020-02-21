c7e6e1558eecec3aa620561a92ae136afe6c5d2d
BingoZhou <bingoabs@gmail.com>
2020-02-21 17:53:34 +0800

    six array example

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <cctype>
+#include <iterator>
 using std::vector;
 using std::string;
 using std::cin;
@@ -10,17 +11,26 @@ using std::endl;
 
 int main()
 {
-    int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int *p = arr;// head ptr of arr
-    ++p;
-    int *e = &arr[10]; // end + 1 ptr of arr
-    for(int *b = arr; b != e; ++b)
+    int ia[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int *p0 = ia;
+    int *p1 = &ia[2];
+    int *p2 = &ia[3];
+    p1 += p2 - p1;
+    cout << *p1 << " " << "p1" << endl;
+
+    for(auto a : ia)
     {
-        cout << *b << endl;
+        cout << a << " ";
     };
-    cout << "next one" << endl;
-    for(int *b = arr; b != &arr[9]; ++b)
+    cout << endl;
+    
+    for(int *beg = ia; beg < ia + 10; ++beg)
     {
-        cout << *b << endl;
-    }
+        *beg = 0;
+    };
+    for(auto a : ia)
+    {
+        cout << a << " ";
+    };
+    cout << endl;
 }
