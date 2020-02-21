57842fa9ae919619b46f7841ee665f453624ccaa
BingoZhou <bingoabs@gmail.com>
2020-02-21 15:53:21 +0800

    third array example

--- main.cc
+++ main.cc
@@ -10,15 +10,15 @@ using std::endl;
 
 int main()
 {
-    unsigned scores[11] = {};
-    unsigned grade;
-    while(cin >> grade){
-        if(grade <= 100)
-        {
-            ++scores[grade/10];
-        };
+    int a[10] = {};
+    int beg = 0;
+    for(int &e : a)
+    {
+        e = beg++;
     };
-    for (auto i : scores)
-        cout << i << " ";
+    for(int e : a)
+    {
+        cout << e << " ";
+    };
     cout << endl;
 }
