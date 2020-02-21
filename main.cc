1c92ed16c1af9e583ef88fa8a74ac372e8011a12
BingoZhou <bingoabs@gmail.com>
2020-02-21 22:09:30 +0800

    loop nested array

--- main.cc
+++ main.cc
@@ -21,5 +21,15 @@ int main()
     int arr[10][20][30] = {0};// set all elements to 0!
 
     ia[2][3] = arr[0][0][0];
-    int (&row)[4] = ia[1];// bind the `row` to a array which contains 4 int elements
+    int (&row)[4] = ia[1];
+    // bind the `row` to a array which contains 4 int elements
+    constexpr size_t rowCnt = 3, colCnt = 4;
+    int ia[rowCnt][colCnt];
+    for(size_t i = 0; i != rowCnt; ++i)
+    {
+        for(size_t j = 0; j != colCnt; ++j)
+        {
+            ia[i][j] = i * colCnt + j;
+        }
+    }
 }
