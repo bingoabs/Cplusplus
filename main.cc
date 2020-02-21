e4eac9bdc236eb8f0967f03786584f73933ec6aa
BingoZhou <bingoabs@gmail.com>
2020-02-21 15:51:15 +0800

    second array example

--- main.cc
+++ main.cc
@@ -10,23 +10,15 @@ using std::endl;
 
 int main()
 {
-    unsigned cnt = 42;
-    constexpr unsigned sz = 42;
-    //const unsigned sz = 100;
-    int arr[10];
-    int *parr[sz]; // right, as the `sz` is the constexpr
-    //string bad[cnt];  error, the cnt not const or constexpr
-    //string strs[get_size()]; error if the get_size() is not a constexpr
-
-    const unsigned sz = 3;
-    int ial[sz] = {0, 1, 2};
-    int a2[] = {0, 1, 2};
-    int a3[5] = {0, 1, 2}; // equals to a3[] = {0, 1, 2, 0, 0};
-    string a4[3] = {"hi", "bye"}; // equals to a4[] = {"hi", "bye", ""};
-
-    char a1[] = {'c', '+', '+'};
-    char a2[] = {'c', '+', '+', '\0'};
-    char a3[] = "c++"; // equals to a3[] = {'c', '+', '+', '\0'};
-    //const char a4[6] = "Daniel";
-    // error, as the char array need one more space to store the "Zero"
+    unsigned scores[11] = {};
+    unsigned grade;
+    while(cin >> grade){
+        if(grade <= 100)
+        {
+            ++scores[grade/10];
+        };
+    };
+    for (auto i : scores)
+        cout << i << " ";
+    cout << endl;
 }
