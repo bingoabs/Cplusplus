0a552b6cf3fd6ae3fc1e74a21bbe83a5ce73e650
BingoZhou <bingoabs@gmail.com>
2020-02-24 00:23:47 +0800

    cast:

--- main.cc
+++ main.cc
@@ -11,10 +11,18 @@ using std::endl;
 // typedef int int_array[4]; old way to get an alias
 int main()
 {
-    // int x[10]; int *p = x;
-    // cout << sizeof(x)/sizeof(*x) << endl;
-    // cout << sizeof(p)/sizeof(*p) << endl;
-    int x = 10, y = 20;
-    cout << (sizeof x + y) << endl;
-    cout << (sizeof x < y) << endl;
+    // vector<int> ivec = {1, 2, 3, 4, 5};
+    // vector<int>::size_type cnt = ivec.size();
+    // for(vector<int>::size_type ix = 0;
+    //     ix != ivec.size(); ix++, cnt--)
+    //     ivec[ix] = cnt;
+    // for(auto x : ivec)
+    //     cout << x << " ";
+    // cout << endl;
+    int x = 10, y = 15;
+    int input;
+    cin >> input;
+    cout << ((input > 10) ? (++x, ++y) : --x, --y) << endl;
+    cout << x << " " << y << endl;
+    cout << ++y << endl;
 }
