15bb1d2beddf4dac9af5df9a47b4bc27abda99b4
BingoZhou <bingoabs@gmail.com>
2020-02-23 12:32:52 +0800

    finish array pratice

--- main.cc
+++ main.cc
@@ -7,7 +7,8 @@ using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
-
+// using int_array = int[4]; new way to get an alias
+// typedef int int_array[4]; old way to get an alias
 int main()
 {
     int ia[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
