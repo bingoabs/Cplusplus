3d98d1569b3c964d60b69c42bb291cdfcd75bb63
BingoZhou <bingoabs@gmail.com>
2020-02-24 00:46:13 +0800

    cast operator

--- main.cc
+++ main.cc
@@ -11,18 +11,24 @@ using std::endl;
 // typedef int int_array[4]; old way to get an alias
 int main()
 {
-    // vector<int> ivec = {1, 2, 3, 4, 5};
-    // vector<int>::size_type cnt = ivec.size();
-    // for(vector<int>::size_type ix = 0;
-    //     ix != ivec.size(); ix++, cnt--)
-    //     ivec[ix] = cnt;
-    // for(auto x : ivec)
-    //     cout << x << " ";
-    // cout << endl;
-    int x = 10, y = 15;
-    int input;
-    cin >> input;
-    cout << ((input > 10) ? (++x, ++y) : --x, --y) << endl;
-    cout << x << " " << y << endl;
-    cout << ++y << endl;
+    // int j = 10, i = 99;
+    // double slope = static_cast<double>(j) / i;
+
+    // double d = 100.0;
+    // void* p = &d;
+    // double *dp = static_cast<double*>(p);
+
+    // const char *pc;
+    // char *p = const_cast<char*>(pc);
+
+    // int *ip;
+    // char *pc = reinterpret_cast<char*>(ip);
+    // the pc point to a `int`, but we can change the point type to `char`
+    // treat `pc` as a real char ptr may raise error, like:
+    // string str(pc); would raise error in runtime
+
+    double d = 1.2343;
+    int i = 123;
+    i*=static_cast<int>(d);
+    cout << i << endl;
 }
