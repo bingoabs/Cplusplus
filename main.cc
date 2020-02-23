b679267b145c9196cf4ffba100205b0d75b7b667
BingoZhou <bingoabs@gmail.com>
2020-02-23 13:02:15 +0800

    operator example

--- main.cc
+++ main.cc
@@ -11,21 +11,21 @@ using std::endl;
 // typedef int int_array[4]; old way to get an alias
 int main()
 {
-    int ia[3][4] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-    int (*p)[4] = ia;
-    p = &ia[2];
-
-    for (int (*p)[4] = ia; p != ia + 3; ++p)
-    {
-        for(int *q = *p; q != *p + 4; ++q)
-            cout << *q << ' ';
-        cout << endl;
+    int ival = 42;
+    double dval = 3.14;
+    ival % 12;
+    //ival % dval; error
+    vector<string> text = {"test", "test"};
+    for(const auto &s : text){
+        cout << s;
+        if (s.empty() || s[s.size() - 1] == '.')
+            cout << endl;
+        else
+        {
+            cout << " ";
+        }
     };
-
-    // for (auto p = begin(ia); p != end(ia); ++p)
-    // {
-    //     for (auto q = begin(*p); q != en(*p); ++q)
-    //         cout << *q << ' ';
-    //     cout << endl;
-    // };
+    cout << endl;
+    const char *cp = "Hello World";
+    // generate a char array?
 }
