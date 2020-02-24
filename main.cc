439a0f1708ef728d2667939eb7958d2f7b18f565
BingoZhou <bingoabs@gmail.com>
2020-02-24 23:46:09 +0800

    try catch example

--- main.cc
+++ main.cc
@@ -2,23 +2,27 @@
 #include <string>
 #include <iostream>
 #include <cctype>
+#include <stdexcept>
+
 using std::vector;
 using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::runtime_error;
 // using int_array = int[4]; new way to get an alias
 // typedef int int_array[4]; old way to get an alias
 int main()
 {
-    vector<int> a, b;
-    a = {1, 2, 3, 4, 5, 6, 7, 8};
-    // b = {1, 2, 3, 4, 5, 6};
-    b = {1, 2, 3, 4, 5, 5};
-    bool isPrefix = true;
-    for(auto ab = a.begin(), bb = b.begin(); ab != a.end() && bb != b.end(); ++ab, ++bb){
-        if(*ab != *bb)
-            isPrefix = false;
+    try {
+        string a;
+        cin >> a;
+        if (a != "test")
+            throw runtime_error("test");
+        cout << "no error" << endl;
+    } catch (runtime_error err){
+        cout << err.what()
+            << endl
+            << "this is the error message" << endl;
     }
-    cout << isPrefix << endl;
 }
