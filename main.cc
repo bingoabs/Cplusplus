401b2fff0678140b9d5b69b820ad836ef9d571a2
BingoZhou <bingoabs@gmail.com>
2020-02-24 23:28:44 +0800

    switch example

--- main.cc
+++ main.cc
@@ -11,24 +11,23 @@ using std::endl;
 // typedef int int_array[4]; old way to get an alias
 int main()
 {
-    // int j = 10, i = 99;
-    // double slope = static_cast<double>(j) / i;
-
-    // double d = 100.0;
-    // void* p = &d;
-    // double *dp = static_cast<double*>(p);
-
-    // const char *pc;
-    // char *p = const_cast<char*>(pc);
-
-    // int *ip;
-    // char *pc = reinterpret_cast<char*>(ip);
-    // the pc point to a `int`, but we can change the point type to `char`
-    // treat `pc` as a real char ptr may raise error, like:
-    // string str(pc); would raise error in runtime
-
-    double d = 1.2343;
-    int i = 123;
-    i*=static_cast<int>(d);
-    cout << i << endl;
+    const unsigned ival = 512, jval = 1024, kval = 4096;
+    unsigned bufsize;
+    unsigned swt;
+    cin >> swt;
+    switch (swt) {
+        case ival:
+            cout << "ival" << endl;
+            break;
+        case jval:
+            cout << "jval" << endl;
+            break;
+        case kval:
+            cout << "kval" << endl;
+            break;
+        default:
+            cout << "default" << endl;
+            break;
+    }
+    cout << "end" << endl;
 }
