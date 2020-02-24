e72b057e87c879a406112632d6f7687e31f4326f
BingoZhou <bingoabs@gmail.com>
2020-02-24 23:41:31 +0800

    5.4.2 practice

--- main.cc
+++ main.cc
@@ -11,16 +11,14 @@ using std::endl;
 // typedef int int_array[4]; old way to get an alias
 int main()
 {
-    string input, last = "";
-    int cnt = 1;
-    while (cin >> input) {
-        if (input == last){
-            ++cnt;
-        }
-        else {
-            cout << "input:" << " '" << last << "'" << " " << cnt << " times" << endl;
-            last = input;
-            cnt = 1;
-        }
+    vector<int> a, b;
+    a = {1, 2, 3, 4, 5, 6, 7, 8};
+    // b = {1, 2, 3, 4, 5, 6};
+    b = {1, 2, 3, 4, 5, 5};
+    bool isPrefix = true;
+    for(auto ab = a.begin(), bb = b.begin(); ab != a.end() && bb != b.end(); ++ab, ++bb){
+        if(*ab != *bb)
+            isPrefix = false;
     }
+    cout << isPrefix << endl;
 }
