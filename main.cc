f07f7667416bc2cbbf4a888903714767479b19be
BingoZhou <bingoabs@gmail.com>
2020-02-25 17:26:07 +0800

    Update

--- main.cc
+++ main.cc
@@ -12,17 +12,23 @@ using std::endl;
 using std::runtime_error;
 // using int_array = int[4]; new way to get an alias
 // typedef int int_array[4]; old way to get an alias
+
+int fact(int val){
+    int ret = 1;
+    while (val > 1){
+        ret *= val--;
+    }
+    cout << "ret: " << ret << endl;
+    return ret;
+}
+
 int main()
 {
-    try {
-        string a;
-        cin >> a;
-        if (a != "test")
-            throw runtime_error("test");
-        cout << "no error" << endl;
-    } catch (runtime_error err){
-        cout << err.what()
-            << endl
-            << "this is the error message" << endl;
-    }
+    int input;
+    cin >> input;
+    // when the input == 100, the result is wrong
+    // due to the value too large to save in int!
+    cout << "input: " << input << endl;
+    int j = fact(input);
+    cout << "input: " << input << "; result: " << j << endl;
 }
