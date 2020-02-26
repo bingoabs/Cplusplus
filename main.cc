7915a203e5196923671f2f4c71d8b1f193f6076e
BingoZhou <bingoabs@gmail.com>
2020-02-26 22:57:27 +0800

    array ptr

--- main.cc
+++ main.cc
@@ -22,13 +22,61 @@ int fact(int val){
     return ret;
 }
 
+void print(const char *cp)
+{
+    if (cp)
+        while(*cp)
+            cout << *cp++ << endl;
+}
+
+void print(const int *beg, const int *end)
+{
+    while(beg != end)
+        cout << *beg++ << endl;
+}
+
+void print(const int ia[], size_t size)
+{
+    for(size_t i = 0; i < size; ++i)
+    {
+        cout << ia[i] << endl;
+    }
+}
+
+void print(int (&arr)[10])
+// Only the array with ten element can work!
+{
+    for(auto elem : arr)
+    {
+        cout << elem << endl;
+    }
+}
+
+void print(string arr[10])
+// for `array` param, the `10` just for showing, not check the element number
+{
+    size_t size = 5;
+    for(size_t index = 0; index < size; ++index)
+    {
+        cout << arr[index] << endl;
+    }
+    // for (auto elem : arr)
+    // {
+    //     cout << elem << endl;
+    // }
+    // the `arr` looks like a array self,
+    // but actually it is a ptr to array,
+    // the first element in array!
+}
+
 int main()
 {
-    int input;
-    cin >> input;
-    // when the input == 100, the result is wrong
-    // due to the value too large to save in int!
-    cout << "input: " << input << endl;
-    int j = fact(input);
-    cout << "input: " << input << "; result: " << j << endl;
+    //int a[5] = {0, 1, 2, 3, 4};
+    //raised error, because the a[5] doesn't match the `in (&arr)[10]`
+    int intArray[10] = {0, 1, 2, 3, 4};
+    print(intArray);
+
+    string strArray[6] = {"first", "second", "third", "four", "five", "six"};
+    print(strArray);
+    // the definition set the 10, but only 6 element array can work!
 }
