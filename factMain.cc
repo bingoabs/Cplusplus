2726c8c00c432dcf1ecd9e827608e2c73cbd6f96
BingoZhou <bingoabs@gmail.com>
2020-02-26 23:46:03 +0800

    combine input string

--- factMain.cc
+++ factMain.cc
@@ -3,9 +3,22 @@
 
 using std::endl;
 using std::cout;
+using std::string;
 
-int main(){
-    int j = fact(5);
-    cout << "5! is " << j << endl;
+// int main(int argc, char *argv[])
+int main(int argc, char **argv){
+    cout << argc << endl;
+    int index = 1;
+    string resp = "awerewr";
+    cout << resp << endl;
+    while(index <= argc){
+        cout << argv[index++] << endl;
+        //resp += argv[index++];
+        cout << index << endl;
+    }
+    // so werid the resp don't print....
+    // I don't know why
+    cout << resp;
+    cout << endl;
     return 0;
 }
