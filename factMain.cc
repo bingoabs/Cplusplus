309a52608c55080ef9da40c379bd9518fbf878c2
BingoZhou <bingoabs@gmail.com>
2020-02-28 00:23:32 +0800

    function ptr

--- factMain.cc
+++ factMain.cc
@@ -9,14 +9,10 @@ using std::string;
 using std::initializer_list;
 using std::vector;
 
-void f(){ cout << "f()" << endl;}
-void f(int a) {cout << "f(int a)" << endl;}
-void f(int a, int b) {cout << "f(int, int)" << endl;}
-void f(double a, double b = 3.14) {cout << "f(double, double = 3.14)" << endl;}
+typedef int(*first)(int a, int b);
 
 int main(){
-    //f(2.56, 42);
-    f(42);
-    f(42, 0);
-    f(2.56, 3.14);
+    vector<first> b;
+    vector<int(*)(int, int)> a;
+    cout << "end" << endl;
 }
