9cd448b3de2698cda307318af23c75284c076d4e
BingoZhou <bingoabs@gmail.com>
2020-02-28 00:26:01 +0800

    function ptr vector practice

--- factMain.cc
+++ factMain.cc
@@ -11,8 +11,17 @@ using std::vector;
 
 typedef int(*first)(int a, int b);
 
+int plus(int a, int b) { return a + b;}
+int sub(int a, int b){ return a - b;}
+int del(int a, int b){return a/b;}
+int mul(int a, int b){return a * b;}
+
 int main(){
     vector<first> b;
     vector<int(*)(int, int)> a;
+    a.push_back(plus);
+    a.push_back(sub);
+    cout << a[0](10, 20) << endl;
+    cout << a[1](100, 50) << endl;
     cout << "end" << endl;
 }
