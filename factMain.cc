813d436955f7fcce4f7d83ee446cc1bc1316e044
BingoZhou <bingoabs@gmail.com>
2020-03-02 11:47:12 +0800

    class fist practice

--- factMain.cc
+++ factMain.cc
@@ -9,33 +9,48 @@ using std::string;
 using std::initializer_list;
 using std::vector;
 
+class Sales_data {
+    public:
+        Sales_data() = default;
+        Sales_data(const string &s, unsigned n, double p):
+            bookNo(s), units_sold(n), revenue(p*n) {}
+        Sales_data(const string &s):bookNo(s){}
+        Sales_data(std::istream&);
+        string isbn() const{return bookNo;}
+        Sales_data &combine(const Sales_data&);
+        string bookNo;
+        unsigned units_sold = 0;
+        double revenue = 0.0;
+    private:
+        double avg_price() const
+            {return units_sold? revenue/units_sold : 0;}
+};
 
-// Define the function member for Struct/Class
-// The `const` before `{` is used for `this`
-// double Sales_data::avg_price() const {
-//     if (units_sold)
-//         return revenue/units_sold;
-//     else
-//     {
-//         return 0;
-//     }
+std::istream &read(std::istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+Sales_data::Sales_data(std::istream &is)
+{
+    // even the `read` is called by the constructor in Sales_data,
+    // but if the `read` uses the private memeber, the compilation would fail.
+    read(is, *this);
+}
+
+
+// std::ostream &print(std::ostream &os, const Sales_data &item)
+// {
+//     os << item.isbn() << " " << item.units_sold << " "
+//         << item.revenue << " " << item.ave_price();
+//     return os;
 // }
 
-struct People {
-    string name = "name";
-    string address;
-    string get_name() const;
-    string get_address() const;
-};// Notice must have the semicolon !!!
 
-string People::get_name() const {
-    return name;
-}
-string People::get_address() const {
-    return address;
-}
 
 int main(){
-    People first;
-    cout << first.get_name() << endl;
+    return 0;
 }
