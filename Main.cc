23b7526883c22813d8f7807ce10b194be80b1d6f
BingoZhou <bingoabs@gmail.com>
2020-03-03 11:52:46 +0800

    7.27 practice

--- Main.cc
+++ Main.cc
@@ -2,8 +2,9 @@
 #include <iostream>
 
 int main(){
-    Screen myScreen(5, 3);
-    const Screen blank(5, 3);
-    myScreen.set('#').display(std::cout);
-    blank.display(std::cout);
+    Screen myScreen(5, 5, 'X');
+    myScreen.move(4, 0).set('#').display(std::cout);
+    std::cout << "\n";
+    myScreen.display(std::cout);
+    std::cout << "\n";
 }
