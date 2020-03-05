f75c869b758c9169eb5e878c845c4bc32fa4f9d2
BingoZhou <bingoabs@gmail.com>
2020-03-06 00:21:29 +0800

    istream status and reset

--- Main.cc
+++ Main.cc
@@ -1,11 +1,17 @@
 #include "Screen.cc"
 #include <iostream>
 
+/*
+std::istream is a kind of type or object
+std::cin is one object of std::istream
+*/
+void process_input(std::istream& a){
+    std::cout << "this function used to read input";
+}
+
 int main(){
-    Screen::pos ht = 24, wd = 80;
-    Screen scr(ht, wd, ' ');
-    Screen *p = &scr;
-    char c = scr.get();
-    c = p->get();
-    return 0;
+    auto old_state = std::cin.rdstate(); // record the old istream status
+    std::cin.clear(); // make istream work
+    process_input(std::cin); // use the istream 
+    std::cin.setstate(old_state); // set the istream back to old status
 }
