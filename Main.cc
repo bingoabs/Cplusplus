0d3cd3c72595916dff31bf4a6b6241e7f6f976f6
BingoZhou <bingoabs@gmail.com>
2020-03-06 12:25:47 +0800

    using fstream to copy file

--- Main.cc
+++ Main.cc
@@ -3,6 +3,12 @@
 #include <string>
 
 int main(int argc, char *argv[]){
+    // std::ifstream in(ifile);
+    // std:::ofstream out;
+    // out.open(ifile + ".copy");
+    // if (out) // equals to !out.fail()
+    // if open failed, the failbit would be set
+    // so it is a better idea to check the stream status
     std::ifstream input(argv[1]);
     std::ofstream output(argv[2]);
     std::string line = "";
