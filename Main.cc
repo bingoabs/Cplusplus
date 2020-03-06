8645de1a59404775e8695a0108df4500f56fa149
BingoZhou <bingoabs@gmail.com>
2020-03-06 11:11:50 +0800

    io basic usage

--- Main.cc
+++ Main.cc
@@ -1,21 +1,12 @@
 #include "Screen.cc"
 #include <iostream>
-
-/*
-std::istream is a kind of type or object
-std::cin is one object of std::istream
-*/
-std::ostream &process_input(std::istream& a){
-    std::cout << "this function used to read input";
-    std::cout.clear();
-    return std::cout;
-}
+using std::cin;
+using std::cout;
+using std::cerr;
 
 int main(){
-    auto old_state = std::cin.rdstate(); // record the old istream status
-    std::cin.clear(); // make istream work
-    process_input(std::cin); // use the istream 
-    std::cin.setstate(old_state); // set the istream back to old status
-    // std::cin.clear(std::cin.rdstate() & ~std::cin.failbit & ~std::cin.badbit);
-    // reset the `failbit` and `badbit`, but keep the `eofbit`
+    cin.tie(&cout);// just as example, as the tie alread is built by stardard library
+    std::ostream *old_tie = cin.tie(nullptr);
+    cin.tie(&cerr); // tie cin to cerr, but just as example, it is not a good idea
+    cin.tie(old_tie); // rebuild the relation between cin and cout
 }
