b5a05826307a8400861fb0f9528d523728f6a135
BingoZhou <bingoabs@gmail.com>
2020-03-06 12:29:45 +0800

    read file and push into vector and print

--- Main.cc
+++ Main.cc
@@ -1,19 +1,18 @@
 #include "Screen.cc"
 #include <fstream>
 #include <string>
+#include <vector>
+#include <iostream>
 
 int main(int argc, char *argv[]){
-    // std::ifstream in(ifile);
-    // std:::ofstream out;
-    // out.open(ifile + ".copy");
-    // if (out) // equals to !out.fail()
-    // if open failed, the failbit would be set
-    // so it is a better idea to check the stream status
     std::ifstream input(argv[1]);
-    std::ofstream output(argv[2]);
-    std::string line = "";
-    if(getline(input, line))
-        output << line << " the new data" << std::endl;
-    output << "end";
-    return 1;
+    std::vector<std::string> strv;
+    std::string line;
+    while(getline(input, line))
+        strv.push_back(line);
+    for(auto line : strv){
+        std::cout << line << std::endl;
+        std::cout << "-----" << std::endl;
+    }
+    std::cout << "end" << std::endl;
 }
