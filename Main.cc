035b6885527a7fbee29aaee72273625ebd4c42cf
BingoZhou <bingoabs@gmail.com>
2020-03-06 12:06:15 +0800

    simple usage for fstream

--- Main.cc
+++ Main.cc
@@ -1,12 +1,13 @@
 #include "Screen.cc"
-#include <iostream>
-using std::cin;
-using std::cout;
-using std::cerr;
+#include <fstream>
+#include <string>
 
-int main(){
-    cin.tie(&cout);// just as example, as the tie alread is built by stardard library
-    std::ostream *old_tie = cin.tie(nullptr);
-    cin.tie(&cerr); // tie cin to cerr, but just as example, it is not a good idea
-    cin.tie(old_tie); // rebuild the relation between cin and cout
+int main(int argc, char *argv[]){
+    std::ifstream input(argv[1]);
+    std::ofstream output(argv[2]);
+    std::string line = "";
+    if(getline(input, line))
+        output << line << " the new data" << std::endl;
+    output << "end";
+    return 1;
 }
