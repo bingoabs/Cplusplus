b72811bf5db1619fed8657c5d69ccb3a25e45491
BingoZhou <bingoabs@gmail.com>
2020-03-10 00:27:04 +0800

    10.24 practice

--- Main.cc
+++ Main.cc
@@ -16,15 +16,12 @@ using std::placeholders::_1;
 
 bool check_size(const string &s, string::size_type sz)
 {
-    return s.size() >= sz;
+    return s.size() < sz;
 }
 
 int main(int argc, char *argv[]){
     //string words[3] = {"a", "b", "c"};// built-in array has not the iterator!
-    vector<string> words = {"a", "ber", "c", "d"}; 
-    auto wc = std::find_if(words.begin(), words.end(), bind(check_size, _1, 2));
-    for(auto begin = wc; begin != words.end(); ++begin)
-    {
-        cout << *begin << endl;
-    }
+    vector<int> words = {1, 2, 3, 4, 5, 6, 7, 8}; 
+    auto wc = std::find_if(words.begin(), words.end(), bind(check_size, string("word"), _1));
+    cout << *wc << endl;
 }
