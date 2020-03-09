a918dd3da19961b87245a2c56d439e4985919b61
BingoZhou <bingoabs@gmail.com>
2020-03-09 21:57:21 +0800

    lambda example

--- Main.cc
+++ Main.cc
@@ -1,23 +1,16 @@
-#include "Screen.cc"
-#include <fstream>
-#include <string>
+#include <algorithm>
+#include <iostream>
 #include <vector>
-#include <sstream>
+#include <string>
 using std::cout;
 using std::endl;
-
+using std::count_if;
+using std::string;
+using std::vector;
 
 int main(int argc, char *argv[]){
-    size_t v1 = 42;
-    auto f2 = [&v1] { return ++v1;};
-    v1 = 0;
-    auto j = f2();
-    cout << j << endl;
-    auto returnLambda = [](int i) -> int {
-        if (i > 0)
-            return -i;
-        else 
-            return i;
-    };
-    cout << returnLambda(10) << endl;
+    string value(argv[1]);
+    vector<string> a = {"1", "2", "3", "4", "5", "6", "7", "8"};
+    int numberAboveTen = count_if(a.begin(), a.end(), [value](string e) {return e > value;});
+    cout << numberAboveTen << endl;
 }
