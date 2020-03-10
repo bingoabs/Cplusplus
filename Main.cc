05f4d163d042a3c2adf955514dbff89a7f7cbf62
BingoZhou <bingoabs@gmail.com>
2020-03-10 15:21:47 +0800

    9.5.5 practice

--- Main.cc
+++ Main.cc
@@ -4,6 +4,7 @@
 #include <string>
 #include <functional>
 #include <array>
+#include <numeric>
 using std::cout;
 using std::endl;
 using std::count_if;
@@ -14,40 +15,10 @@ using std::placeholders::_1;
 // using namespace std::placeholders;
 // we can directly use the names in this namespace!
 
-string replaceSubstring(string s, string oldVal, string newVal){
-    if(!s.size() || !oldVal.size()){
-        cout << s.size() << " " << oldVal.size();
-        return string("");
-    }
-    string::iterator begin = s.begin();
-    while((begin + oldVal.size()) <= s.end())
-    {
-        string::iterator rawBegin = begin;
-        string::iterator ob = oldVal.begin();
-        for(;ob != oldVal.end(); ob++, begin++)
-        {
-            if(*begin != *ob)
-                break;
-        }
-        if(ob != oldVal.end())
-        {
-            // don't match the oldVal
-            if(ob == oldVal.begin())
-                begin++;
-            continue;
-        }
-        string::iterator iter = s.erase(rawBegin, rawBegin + oldVal.size());
-        iter = s.insert(iter, newVal.begin(), newVal.end());
-        begin = iter + newVal.size();
-    }
-    return s;
-}
-
 int main(int argc, char *argv[]){
-    //.\a.exe test t AAA
-    string s(argv[1]);
-    string oldVal(argv[2]);
-    string newVal(argv[3]);
-    string sa = replaceSubstring(s, oldVal, newVal);
-    cout << sa << endl;
+    vector<int> nums;
+    vector<string> strings = {"1", "2", "3", "4"};
+    for(string str : strings)
+        nums.push_back(std::stoi(str));
+    cout << std::accumulate(nums.begin(), nums.end(), 0) << endl;
 }
