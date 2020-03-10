ee36f6c23bd4c23dee54d45cc7f7b9527d11b5d1
BingoZhou <bingoabs@gmail.com>
2020-03-10 17:48:23 +0800

    9.25 pratice

--- Main.cc
+++ Main.cc
@@ -5,6 +5,7 @@
 #include <functional>
 #include <array>
 #include <numeric>
+#include <list>
 using std::cout;
 using std::endl;
 using std::count_if;
@@ -12,20 +13,39 @@ using std::string;
 using std::vector;
 using std::bind;
 using std::placeholders::_1;
+using std::list;
 // using namespace std::placeholders;
 // we can directly use the names in this namespace!
 
-class Example {
-    public:
-        static double rate = 6.5;
-        static constexpr int vecSize = 20;
-        static vector<double> vec(10);
-};
 int main(int argc, char *argv[]){
-    int vec = 10;
-    vector<int> nums(vec);
-    vector<string> strings = {"1", "2", "3", "4"};
-    for(string str : strings)
-        nums.push_back(std::stoi(str));
-    cout << std::accumulate(nums.begin(), nums.end(), 0) << endl;
+    int a[] = {1, 2, 3, 4, 5};
+    vector<int> vec(std::begin(a), std::end(a));
+    list<int> li(std::begin(a), std::end(a));
+    vector<int>::iterator iter = vec.begin();
+    while(iter != vec.end())
+    {
+        if(!(*iter%2))
+            iter = vec.erase(iter);
+        else
+        {
+            iter++;
+        }
+    }
+    list<int>::iterator liter = li.begin();
+    while(liter != li.end())
+    {
+        if(*liter%2)
+            liter = li.erase(liter);
+        else
+        {
+            liter++;
+        }
+        
+    }
+    for(auto a : vec)
+        cout << a << " ";
+    cout << endl;
+    for(auto b : li)
+        cout << b << " ";
+    cout << endl;
 }
