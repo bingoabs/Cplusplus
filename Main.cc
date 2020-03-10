ee5002b7536a0c0ba62cda802591ef59513087dc
BingoZhou <bingoabs@gmail.com>
2020-03-10 14:26:12 +0800

    9.43 practice

--- Main.cc
+++ Main.cc
@@ -14,9 +14,50 @@ using std::placeholders::_1;
 // using namespace std::placeholders;
 // we can directly use the names in this namespace!
 
+string replaceSubstring(string s, string oldVal, string newVal){
+    if(!s.size() || !oldVal.size()){
+        cout << s.size() << " " << oldVal.size();
+        return string("");
+    }
+    bool inSub = false;
+    vector<int> startIndex;
+    // collect all the index of `s` which store the first element in `oldVal`
+    for(int index = 0; index < s.size(); ++index)
+    {
+        if (s[index] == oldVal[0])
+            startIndex.push_back(index);
+    }
+    vector<int> realSubIndex;
+    while(startIndex.size() > 0)
+    {
+        if((s.size() - startIndex.back() + 1) < (oldVal.size()))
+        {
+            continue;
+        }
+        bool isSub = true;
+        for(int startS = startIndex.back(), startOld = 0;
+            startOld < oldVal.size(); // oldVal is the limit
+            ++startS, ++startOld)
+        {
+            if (s[startS] != oldVal[startOld])
+                isSub = false;
+        }
+        if(isSub)
+            realSubIndex.push_back(startIndex.back());
+        startIndex.pop_back();
+    }
+    for(int index : realSubIndex)
+    {
+        s.replace(index, oldVal.size(), newVal);
+    }
+    return s;
+
+}
 
 int main(int argc, char *argv[]){
-    vector<char> vchar = {'a', 'b', 'c', 'd'};
-    string s(vchar.begin(), vchar.end());
-    cout << s << endl;
+    string s(argv[1]);
+    string oldVal(argv[2]);
+    string newVal(argv[3]);
+    string sa = replaceSubstring(s, oldVal, newVal);
+    cout << sa << endl;
 }
