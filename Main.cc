95709d73744d785019123a637a8b69f85b9eac9f
BingoZhou <bingoabs@gmail.com>
2020-03-10 09:56:04 +0800

    9.41 practice

--- Main.cc
+++ Main.cc
@@ -16,18 +16,7 @@ using std::placeholders::_1;
 
 
 int main(int argc, char *argv[]){
-    vector<int> loopNums = {256, 512, 1000, 1048};
-    for(int maxIndex : loopNums)
-    {
-        cout << "Max Index value is: " << maxIndex << endl;
-        vector<string> records;
-        records.reserve(1024);
-        cout << "begin " << "size: " << records.size() << " " << "capacity: " << records.capacity() << endl;
-        for(int index = 0; index < maxIndex; ++index)
-            records.push_back("records");
-        cout << "after " << "size: " << records.size() << " " << "capacity: " << records.capacity() << endl;
-        records.resize(records.size() + records.size()/2);
-        cout << "end " << "size: " << records.size() << " " << "capacity: " << records.capacity() << endl;
-        cout << endl << endl;
-    }
+    vector<char> vchar = {'a', 'b', 'c', 'd'};
+    string s(vchar.begin(), vchar.end());
+    cout << s << endl;
 }
