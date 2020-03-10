6b07b4e552bb9bfedd5298dc3ba532df9b72d3f8
BingoZhou <bingoabs@gmail.com>
2020-03-10 09:30:50 +0800

    9.39 practice

--- Main.cc
+++ Main.cc
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 #include <functional>
-
+#include <array>
 using std::cout;
 using std::endl;
 using std::count_if;
@@ -14,14 +14,20 @@ using std::placeholders::_1;
 // using namespace std::placeholders;
 // we can directly use the names in this namespace!
 
-bool check_size(const string &s, string::size_type sz)
-{
-    return s.size() < sz;
-}
 
 int main(int argc, char *argv[]){
-    //string words[3] = {"a", "b", "c"};// built-in array has not the iterator!
-    vector<int> words = {1, 2, 3, 4, 5, 6, 7, 8}; 
-    auto wc = std::find_if(words.begin(), words.end(), bind(check_size, string("word"), _1));
-    cout << *wc << endl;
+    vector<int> loopNums = {256, 512, 1000, 1048};
+    for(int maxIndex : loopNums)
+    {
+        cout << "Max Index value is: " << maxIndex << endl;
+        vector<string> records;
+        records.reserve(1024);
+        cout << "begin " << "size: " << records.size() << " " << "capacity: " << records.capacity() << endl;
+        for(int index = 0; index < maxIndex; ++index)
+            records.push_back("records");
+        cout << "after " << "size: " << records.size() << " " << "capacity: " << records.capacity() << endl;
+        records.resize(records.size() + records.size()/2);
+        cout << "end " << "size: " << records.size() << " " << "capacity: " << records.capacity() << endl;
+        cout << endl << endl;
+    }
 }
