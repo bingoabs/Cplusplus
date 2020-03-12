b3d5b02abe7c85b6fcbaadaa43f6261417f746e8
BingoZhou <bingoabs@gmail.com>
2020-03-12 10:44:41 +0800

    10.25 practice

--- Main.cc
+++ Main.cc
@@ -15,18 +15,65 @@ using std::string;
 using std::vector;
 using std::bind;
 using std::placeholders::_1;
+using std::placeholders::_2;
 using std::list;
 using std::stack;
-// using namespace std::placeholders;
-// we can directly use the names in this namespace!
+void elimDups(vector<string> &words)
+{
+    std::sort(words.begin(), words.end());
+    auto end_unique = std::unique(words.begin(), words.end());
+    words.erase(end_unique, words.end());
+}
+
+bool isBig(string a, vector<string>::size_type b) 
+{
+    return a.size() > b;
+}
+void print(const string &s){ cout << s << endl;}
+
+void displayUsingFindif(vector<string> &words, vector<string>::size_type sz)
+{
+    // using find_if, return the first one which are true
+    // auto wc = std::find_if(words.begin(), words.end(),
+    // [sz](const string &a)
+    // {return a.size() >= sz;});
+    auto wc = std::find_if(words.begin(), words.end(), std::bind(isBig, _1, sz));
+    auto count = words.end() - wc;
+    cout << count << " " << " of length " << sz << " or longer" << endl;
+    // std::for_each(wc, words.end(), [](const string &s){cout << s << " ";});
+    std::for_each(wc, words.end(), std::bind(print, _1));
+
+}
+
+// void displayUsingPartition(vector<string> &words, vector<string>::size_type sz)
+// {
+//     // using partition, put the `true` element in the head, 
+//     // return the first iterator which point to the first `false` element.
+//     // auto wc = std::partition(words.begin(), words.end(), [sz](const string &a) {return a.size() >> sz;});
+//     auto wc = std::partition(words.begin(), words.end(), std::bind(isBig, _1, _2));
+//     cout << endl;
+//     auto count = words.begin() - wc; 
+//     cout << count << " " << " of length " << sz << " or longer" << endl;
+//     // std::for_each(words.begin(), wc, [](const string &s) {cout << s << " ";});
+//     std::for_each(words.begin(), wc, std::bind(print, _1));
+// }
+
+void biggies(vector<string> &words,
+vector<string>::size_type sz)
+{
+    elimDups(words);
+    std::stable_sort(words.begin(), words.end(),
+        [](const string &a, const string &b)
+        {
+            return a.size() < b.size();
+        });
+    displayUsingFindif(words, sz);
+}
 
 int main(int argc, char *argv[]){
-    vector<int> vec = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    //vec.reserve(10);
-    // fill_n and copy requires the target array to 
-    // have enough space to store the data
-    fill_n(vec.begin(), 10, 0);
-    for(auto a : vec)
+    vector<string> words = {"b", "a", "c", "d", "a", "b"};
+    biggies(words, words.size());
+    for(auto a : words)
         cout << a << " ";
-    cout << "end" << endl;
+    cout << endl;
 }
