233322ac5d0b49c1d36bda7b94ac3916679ff7a4
BingoZhou <bingoabs@gmail.com>
2020-03-12 16:49:11 +0800

    10.41 practice

--- Main.cc
+++ Main.cc
@@ -22,19 +22,11 @@ using std::list;
 using std::stack;
 
 int main(int argc, char *argv[]){
-    std::istream_iterator<int> in(cin);
-    std::istream_iterator<int> in_eof;
-    // It looks like one istream_iterator can't reuse
-    // vector<int> vn(in, in_eof);
-    // for(auto v : vn)
-    //     cout << v << " ";
-    // cout << endl;
-    vector<int> vn2(10, 1);
-    auto end = std::copy(in, in_eof, vn2.begin());
-    
-    for(auto v : vn2)
-        cout << v << " ";
-    cout << endl;
-    cout << *end << endl;
-    cout << (vn2.begin() - end) << endl;
+    string raw = "this is the test";
+    char o = 'i';
+    char n = 'I';
+    string r;
+    std::replace_copy_if(raw.begin(), raw.end(), std::back_inserter(r), [](char a){return a == 't';}, 'T');
+    for(char a : r)
+        cout << a << endl;
 } 
