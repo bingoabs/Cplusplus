f04b1492e5f83c033820cece9b43dab0d53e7a7b
BingoZhou <bingoabs@gmail.com>
2020-03-18 23:34:31 +0800

    new and delete memory

--- Main.cc
+++ Main.cc
@@ -31,25 +31,6 @@ using std::multiset;
 
 int main(int argc, char *argv[])
 {
-    multiset<string> c = {"a", "b", "c"};
-    vector<string> v = {"d", "e", "f"};
-    // std::copy(v.begin(), v.end(), std::inserter(c, c.end()));
-    // std::copy(v.begin(), v.end(), std::back_inserter(c)); // only this one failed
-    // std::copy(c.begin(), c.end(), std::inserter(v, v.end()));
-    std::copy(c.begin(), c.end(), std::back_inserter(v));
-    multiset<string>::iterator cb = c.begin();
-    while(cb != c.end())
-    {
-        cout << *cb << " ";
-        cb++;
-    }
-    cout << endl;
-    for(auto a : v)
-        cout << a << " ";
-    cout << endl;
-    set<int> iset = {0, 1, 2, 3, 4, 5, 6};
-    iset.find(1); // return a set<int>::iterator reference to the key == 1 element
-    iset.find(11); // return a set<int>::iterator reference to the one after tail
-    iset.count(1); // return 1;
-    iset.count(11); // return 0;
+    const int *pci = new const int(1024);
+    delete pci;
 } 
