255569aaa171a9556417481d53186b3975604860
BingoZhou <bingoabs@gmail.com>
2020-03-19 11:43:51 +0800

    not return shared_ptr

--- Main.cc
+++ Main.cc
@@ -30,13 +30,12 @@ using std::map;
 using std::set;
 using std::multiset;
 
-std::shared_ptr<vector<int>> recv(std::shared_ptr<vector<int>> p)
+void recv(std::shared_ptr<vector<int>> p)
 {
     cout << "2.recv: " << p.use_count() << endl;
     int input;
     while(cin >> input)
         p->push_back(input);
-    return p;
 }
  
 void print(std::shared_ptr<vector<int>> p)
@@ -51,7 +50,7 @@ int main(int argc, char *argv[])
 {
     std::shared_ptr<vector<int>> a = std::make_shared<vector<int>>();
     cout << "1: " << a.use_count() << endl;
-    a = recv(a);
+    recv(a);
     cout << "2: " << a.use_count() << endl;
     print(a);
     cout << "3: " << a.use_count() << endl;
