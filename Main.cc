10f08caef307f4e7fc7744659d954f3447349a2d
BingoZhou <bingoabs@gmail.com>
2020-03-22 20:40:26 +0800

    copy constructor

--- Main.cc
+++ Main.cc
@@ -35,80 +35,22 @@ using std::unique_ptr;
 using std::allocator;
 using std::ifstream;
 
-class QueryResult;
-class TextQuery {
-    public:
-        using line_no = std::vector<string>::size_type;
-        TextQuery(std::ifstream&);
-        QueryResult query(const std::string&) const;
-    private:
-        std::shared_ptr<std::vector<std::string>> file;
-        std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;                   
+class Sales_data {
+public:
+    Sales_data(const Sales_data&);
+private:
+    std::string bookNo;
+    int units_sold = 0;
+    double revenue = 0.0;
 };
 
-TextQuery::TextQuery(ifstream &is):file(new vector<string>)
-{
-    string text;
-    while(getline(is, text))
-    {
-        file->push_back(text);
-        int n = file->size() - 1;
-        std::istringstream line(text);
-        string word;
-        while(line >> word)
-        {
-            auto &lines = wm[word];
-            if(!lines)
-                lines.reset(new setMline_no>);
-            lines->insert(n);
-        }
-    }
-}
-
-class QueryResult {
-    friend std::ostream& print(std::ostream&, const QueryResult&);
-    public:
-        QueryResult(std::string s,
-            std::shared_ptr<std::set<line_no>> p,
-            std::shared_ptr<std::vector<std::string>> f):
-        sought(s), lines(p), file(f){};
-    private:
-        std::string sought;
-        std::shared_ptr<std::set<line_no>> lines;
-        std::shared_ptr<std::vector<std::string>> file;
-};
-
-QueryResult
-TextQuery::query(const string &sought) const
-{
-    static shared_ptr<set<line_no>> nodata(new set<line_no>);
-    auto loc = wm.find(sought);
-    if(loc == wm.end())
-        return QueryResult(sought, nodata, file);
-    else
-        return QueryResult(sought, loc->second, file);
-}
-
-void runQueries(ifstream &infile)
-{
-    TextQuery tq(infile);
-    while(true){
-        cout << "enter word to look for, or q to quit:";
-        string s;
-        if(!(cin >> s) || s == "q") break;
-        print(cout, tq.query(s)) << endl;
-    }
-}
-
-ostream &print(ostream & os, const QueryResult &qr)
-{
-    os << qr.sought << " occurs " << qr.lines->size() << " " << endl;
-    for(auto num : *qr.lines)
-        os << "\t(line " << num + 1 << ")" << *(qr.file->begin() + num) << endl;
-    return os;
-}
+Sales_data::Sales_data(const Sales_data &orid):
+    bookNo(orid.bookNo),
+    units_sold(orid.units_sold),
+    revenue(orid.revenue)
+    { }
 
 int main(int argc, char *argv[])
 {
-
+    return 0;
 } 
