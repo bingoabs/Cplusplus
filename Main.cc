c8fe6543ab4cbba215c3aa1a8381fab219a5a63e
BingoZhou <bingoabs@gmail.com>
2020-04-05 15:04:31 +0800

    14.51

--- Main.cc
+++ Main.cc
@@ -10,18 +10,30 @@ using std::placeholders::_1;
 struct LongDouble{
     LongDouble(double c = 0.0):a(c){};
     operator double(){
+        std::cout << "ld double" << std::endl;
         return 1.0;
     };
     operator float(){
+        std::cout << "ld float" << std::endl;
         return 2.0;
     }
     double a;
 };
 
+void calc(int a)
+{
+    std::cout << "cal int" << std::endl;
+    return;
+}
+
+void calc(LongDouble a)
+{
+    std::cout << "calc ld" << std::endl;
+    return ;
+}
 int main(int argc, char *argv[])
 {
-    LongDouble ldObj;
-    //int ex1 = ldObj;
-    float ex2 = ldObj;
+    double dval;
+    calc(dval);
     return 0;
 } 
