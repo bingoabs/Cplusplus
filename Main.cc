66929f61bd0321aca4bf31a1a4c3602d7c3ed8db
BingoZhou <bingoabs@gmail.com>
2020-04-05 14:01:14 +0800

    14.9.1

--- Main.cc
+++ Main.cc
@@ -3,6 +3,8 @@
 #include <string>
 #include <algorithm>
 #include <functional>
+
+using std::placeholders::_1;
 #include "Message.cc"
 
 class getLen{
@@ -12,13 +14,19 @@ class getLen{
         {
             return (str.size() == num);
         }
+        operator int() const {
+            return num;
+        }
     private:
         int num;
 };
 
 int main(int argc, char *argv[])
 {
-    std::vector<std::string> ivec = {"a", "ab", "abc", "abcd", "abcde"};
-    //std::count_if(ivec.cbegin(), ivec.cend(), std::bind(std::greater<int>(), _1, 1024));
+    std::vector<int> ivec = {1, 2, 3, 4, 5, 6, 7};
+    std::count_if(ivec.cbegin(), ivec.cend(), std::bind(std::greater<int>(), _1, 1024));
+    getLen a(100);
+    int r = a;
+    std::cout << r << std::endl;
     return 0;
 } 
