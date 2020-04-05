511dd805b9216e15075fadeaeec9f8cd2b94c9d7
BingoZhou <bingoabs@gmail.com>
2020-04-05 14:59:50 +0800

    14.50

--- Main.cc
+++ Main.cc
@@ -7,26 +7,21 @@
 using std::placeholders::_1;
 #include "Message.cc"
 
-class getLen{
-    public:
-        getLen(const int i):num(i){}
-        bool operator()(const std::string str)
-        {
-            return (str.size() == num);
-        }
-        operator int() const {
-            return num;
-        }
-    private:
-        int num;
+struct LongDouble{
+    LongDouble(double c = 0.0):a(c){};
+    operator double(){
+        return 1.0;
+    };
+    operator float(){
+        return 2.0;
+    }
+    double a;
 };
 
 int main(int argc, char *argv[])
 {
-    std::vector<int> ivec = {1, 2, 3, 4, 5, 6, 7};
-    std::count_if(ivec.cbegin(), ivec.cend(), std::bind(std::greater<int>(), _1, 1024));
-    getLen a(100);
-    int r = a;
-    std::cout << r << std::endl;
+    LongDouble ldObj;
+    //int ex1 = ldObj;
+    float ex2 = ldObj;
     return 0;
 } 
