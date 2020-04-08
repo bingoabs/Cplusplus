b23f0b5657393fe56c04509b4cb53d342d24ec2d
BingoZhou <bingoabs@gmail.com>
2020-04-08 14:20:39 +0800

    friend inherit

--- Main.cc
+++ Main.cc
@@ -1,26 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <functional>
-
 class Base {
+    friend class Pal;
+    int test;
     protected:
         int prot_mem;
 };
 
-class Sneaky : public Base {
+class Sneaky : public Base { // private or protected [Base] would raise error!
     friend void clobber(Sneaky&);
     friend void clobber(Base&);
+    int j;
 };
 
-void clobber(Sneaky &s){
-    s.j = s.prot_mem = 0;
-}
+class Pal {
+    public:
+        int f(Base b){return b.prot_mem;}
+        int ff(Base b) { return b.test;};
+        // friend can visit the private member
+        // ok: Pal is a friend of Base
+        // int f2(Sneaky s){return s.j;} 
+        // error: Pal not friend of Sneaky
+        // access to a base class is controlled by the base class, even inside a derived object
+        int f3(Sneaky s) { return s.prot_mem;}
+        // ok: Pal is a friend;
+};
 
-void clobber(Base &s){
-    s.prot_mem = 0;
-}
 
 int main(int argc, char *argv[])
 {
