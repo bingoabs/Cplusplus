e0d4427693e9d34772f6a3400a3925834736a8dc
BingoZhou <bingoabs@gmail.com>
2020-04-08 13:53:51 +0800

    class inherite

--- Main.cc
+++ Main.cc
@@ -4,36 +4,25 @@
 #include <algorithm>
 #include <functional>
 
-using std::placeholders::_1;
-#include "Message.cc"
-
-struct LongDouble{
-    LongDouble(double c = 0.0):a(c){};
-    operator double(){
-        std::cout << "ld double" << std::endl;
-        return 1.0;
-    };
-    operator float(){
-        std::cout << "ld float" << std::endl;
-        return 2.0;
-    }
-    double a;
+class Base {
+    protected:
+        int prot_mem;
 };
 
-void calc(int a)
-{
-    std::cout << "cal int" << std::endl;
-    return;
+class Sneaky : public Base {
+    friend void clobber(Sneaky&);
+    friend void clobber(Base&);
+};
+
+void clobber(Sneaky &s){
+    s.j = s.prot_mem = 0;
 }
 
-void calc(LongDouble a)
-{
-    std::cout << "calc ld" << std::endl;
-    return ;
+void clobber(Base &s){
+    s.prot_mem = 0;
 }
+
 int main(int argc, char *argv[])
 {
-    double dval;
-    calc(dval);
     return 0;
 } 
